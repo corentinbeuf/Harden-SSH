15275cb4a625395b237e1812c6d2d5df97d9a139
Corentin BEUF <corentin.beuf.professionnel@proton.me>
2025-11-16 19:29:40 +0100

    Modify function to setup sftp

--- Tools/System-Hardening/sftp-chroot.sh
+++ Tools/System-Hardening/sftp-chroot.sh
@@ -4,33 +4,33 @@ function Setup-SFTPPermission ()
 {
     if ! grep -Fxq "Subsystem sftp internal-sftp" "/etc/ssh/sshd_config"; then
         echo -e "${GREEN}[Task P6] : Implement the principle of least privilege for users using SFTP only - Subsystem.${NC}"
-        sed -i "s/Subsystem       sftp    \/usr\/lib\/openssh\/sftp-server/Subsystem sftp internal-sftp/g" /etc/ssh/sshd_config
+        sed -i '/^Subsystem[[:space:]]\+sftp/d' /etc/ssh/sshd_config
+        sed -i '/# override default of no subsystems/a Subsystem sftp internal-sftp' /etc/ssh/sshd_config
     else
         echo -e "${YELLOW}[Task P6] : SFTP subsystem is already defined${NC}"
     fi
 
-    if ! awk -F':' '/^sftp-users/{print $1}' /etc/group; then
+    if ! getent group sftp-users &>/dev/null; then
         echo -e "${GREEN}[Task P6] : Implement the principle of least privilege for users using SFTP only - Group.${NC}"
-        addgroup sftp-users
+        addgroup sftp-users &>/dev/null
     else
         echo -e "${YELLOW}[Task P6] : SFTP group is already created${NC}"
     fi
 
-    if ! grep -Fxq "Match group sftp-users" "/etc/ssh/sshd_config"; then
+    if ! grep -Fxq "Match Group sftp-users" "/etc/ssh/sshd_config"; then
         echo -e "${GREEN}[Task P6] : Implement the principle of least privilege for users using SFTP only - Permission.${NC}"
         echo "Match Group sftp-users
     ChrootDirectory /sftp-home/%u
     ForceCommand internal-sftp
     AllowTCPForwarding no
-    X11Forwarding no
-    PasswordAuthentication yes" >> /etc/ssh/sshd_config
+    X11Forwarding no" >> /etc/ssh/sshd_config
     else
         echo -e "${YELLOW}[Task P6] : SFTP permissions are already setup${NC}"
     fi
 
-    if [ -d "$DIRECTORY" ]; then
+    if [ ! -d "/sftp-home" ]; then
         echo -e "${GREEN}[Task P6] : Implement the principle of least privilege for users using SFTP only - Directories.${NC}"
-        mkdir /sftp-home
+        mkdir -p /sftp-home
         chown root:root /sftp-home
         chmod 755 /sftp-home
     else

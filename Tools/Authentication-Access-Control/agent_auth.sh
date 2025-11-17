939f50c3852e58892f86df549728982bc4c56fbf
Corentin BEUF <corentin.beuf.professionnel@proton.me>
2025-11-17 13:12:17 +0100

    Comment line to implement ForwardAgent param

--- Tools/Authentication-Access-Control/agent_auth.sh
+++ Tools/Authentication-Access-Control/agent_auth.sh
@@ -9,11 +9,10 @@ function Setup-AuthentificationAgent ()
         echo -e "${YELLOW}[Task R19] : Agent auth are already disabled${NC}"
     fi
 
-    if ! grep -Fxq "ForwardAgent no" "/etc/ssh/sshd_config"; then
-        echo -e "${GREEN}[Task R19] : When SSH bouncing is necessary through a relay host, Agent Forwarding (-A option of ssh) should be used - Forward Agent.${NC}"
-        sed -i '/AllowAgentForwarding no/a ForwardAgent yes' /etc/ssh/sshd_config
-    else
-        echo -e "${YELLOW}[Task R19] : Forward agent param are already disabled${NC}"
-    fi
-    ForwardAgent yes
+#    if ! grep -Fxq "ForwardAgent no" "/etc/ssh/sshd_config"; then
+#        echo -e "${GREEN}[Task R19] : When SSH bouncing is necessary through a relay host, Agent Forwarding (-A option of ssh) should be used - Forward Agent.${NC}"
+#        sed -i '/AllowAgentForwarding no/a ForwardAgent no' /etc/ssh/sshd_config
+#    else
+#        echo -e "${YELLOW}[Task R19] : Forward agent param are already disabled${NC}"
+#    fi
 }
